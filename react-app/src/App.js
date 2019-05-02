da93b7d8d21b3289c0594f7a022b0c99096caf37
정용은 <yong0edu@gmail.com>
2019-05-02 22:10:03 +0900

    State 값을 이용해 props 값 전달. key 사용.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,19 +5,24 @@ import Content from './Components/Content';
 import Subject from './Components/Subject';
 
 
-
-
-
-
-
-
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is ....'},
+        {id:2, title:'CSS', desc:'CSS is ....'},
+        {id:3, title:'JavaScript', desc:'JavaScript is ....'}
+      ]
+    }
+  }
   render(){
     return (
       <div className="App">
-        <Subject title="HTML" sub="World Wide Web!"></Subject>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
         <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is ...."></Content>
       </div>
     );
